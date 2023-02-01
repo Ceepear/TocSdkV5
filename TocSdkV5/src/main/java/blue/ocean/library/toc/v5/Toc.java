401eac40fa584ceae3f00173b5f98faca1f862fb
2791034692 <2791034692@qq.com>
2023-02-01 17:12:50 +0800

    入口 - entrance

---
+++ TocSdkV5/src/main/java/blue/ocean/library/toc/v5/Toc.java
@@ -0,0 +1,6 @@
+package blue.ocean.library.toc.v5;
+import android.content.Context;
+
+public class Toc {
+    public static void init(Context context,TocConfig config){}
+}
---
+++ TocSdkV5/src/main/java/blue/ocean/library/toc/v5/TocConfig.java
@@ -0,0 +1,13 @@
+package blue.ocean.library.toc.v5;
+
+public class TocConfig {
+    public static class Builder{
+        private TocConfig config;
+        public Builder(){
+            config = new TocConfig();
+        }
+        public TocConfig builder(){
+            return config;
+        }
+    }
+}
